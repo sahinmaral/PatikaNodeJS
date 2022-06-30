cd779e94b23beaf21695559a48da2327c4985bf6
sahinmaral <sahin.maral@hotmail.com>
2022-07-01 01:34:05 +0300

    Added getById request to the PCAT and Clean Blog

--- CleanBlog/app.js
+++ CleanBlog/app.js
@@ -1,52 +1,62 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const moment = require('moment')
+const express = require('express');
+const mongoose = require('mongoose');
+const moment = require('moment');
 
-const path = require('path')
+const path = require('path');
 
-const Post = require('./models/Post')
+const Post = require('./models/Post');
 
-mongoose.connect('mongodb://localhost:27017/cleanblog-db')
+mongoose.connect('mongodb://localhost:27017/cleanblog-db');
 
-const app = express()
+const app = express();
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs');
 
-app.use(express.static('public'))
-app.use(express.json())
+app.use(express.static('public'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/',async(req,res)=>{
+app.get('/', async (req, res) => {
+  let posts = await Post.find();
 
-    let posts = await Post.find()
+  posts = posts.map((post) => {
+    return {
+      ...post._doc,
+      dateCreated: moment(post._doc.dateCreated).format('DD/MM/YYYY'),
+    };
+  });
 
-    posts = posts.map(post => {
-        return {
-            ...post._doc , 
-            dateCreated : moment(post._doc.dateCreated).format('DD/MM/YYYY')
-        }
-    })
+  res.render('index', {
+    posts: posts,
+  });
+});
 
-    res.render('index',{
-        posts:posts
-    })
-})
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
-app.get('/about',(req,res)=>{
-    res.render('about')
-})
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
 
-app.get('/add_post',(req,res)=>{
-    res.render('add_post')
-})
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
 
-app.post('/posts',async(req,res)=>{
-    await Post.create(req.body)
-    res.redirect('/')
-})
+app.get('/posts/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id);
 
-const port = 4001
-app.listen(port,()=>{
-    console.log(`App is listening on : ${port}`)
-})
+  res.render('post', {
+    post: {
+      ...post._doc,
+      dateCreated: moment(post._doc.dateCreated).format('DD/MM/YYYY'),
+    },
+  });
+  
+});
 
+const port = 4001;
+app.listen(port, () => {
+  console.log(`App is listening on : ${port}`);
+});
