57bd39400f22b64a6dd163d04df04b6b8c367393
sahinmaral <sahin.maral@hotmail.com>
2022-07-01 14:25:10 +0300

    Added file upload support to PCAT

--- PCAT/app.js
+++ PCAT/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const { v4: uuidv4 } = require('uuid');
 
 const path = require('path');
 const ejs = require('ejs');
+const fs = require('node:fs')
 
 const Photo = require('./models/Photo');
 
@@ -13,17 +16,11 @@ mongoose.connect('mongodb://localhost:27017/pcat-db', {
 
 const app = express();
 
-// Middlewares
-
-// const myLogger = (req,res,next) => {
-//   console.log('Logged')
-//   next()
-// }
-
+app.use(fileUpload());    
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-// app.use(myLogger)
+
 
 // Template Engines
 app.set('view engine', 'ejs');
@@ -46,8 +43,33 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body)
+
+  const uploadDir = 'public/uploads'
+  if (!fs.existsSync(uploadDir)) { 
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+
+  var ext = uploadeImage.name.split('.');
+  const imageName = uuidv4() + '.' + ext[ext.length - 1]
+
+  let uploadPath = __dirname + '/public/uploads/' + imageName;
+  uploadeImage.mv(uploadPath,async(err)=> {
+      await Photo.create({
+        ...req.body,
+        image : '/uploads/' + imageName,
+      })
+  })
+
   res.redirect('/');
+});
+
+app.get('/photos/:id', async(req, res) => {
+  const photo = await Photo.findById(req.params.id)
+  res.render('photo',{
+    photo : photo
+  })
 });
 
 const port = 3000;
